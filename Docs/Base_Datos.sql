d1ca03a1a1f937c6adbf5cf5fc00983b0d55ece2
StevenNoty <steven88724@gmail.com>
2023-12-07 17:51:41 -0500

    Base-Datos

--- Docs/Base_Datos.sql
+++ Docs/Base_Datos.sql
@@ -1,38 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-12-2023 a las 23:49:42
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `Sexto`
+-- Base de datos: `sexto`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `Pais`
+-- Estructura de tabla para la tabla `malla_curricular`
 --
 
-CREATE TABLE `Pais` (
+CREATE TABLE `malla_curricular` (
+  `Id_Materias` int(11) NOT NULL,
+  `Semestre` varchar(255) NOT NULL,
+  `Primera_Materia` varchar(255) NOT NULL,
+  `Segunda_Materia` varchar(255) NOT NULL,
+  `Tercera_Materia` varchar(255) NOT NULL,
+  `Cuarta_Materia` varchar(255) NOT NULL,
+  `Quinta_Materia` varchar(255) NOT NULL,
+  `Sexta_Materia` varchar(255) NOT NULL,
+  `Septima_Materia` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `malla_curricular`
+--
+
+INSERT INTO `malla_curricular` (`Id_Materias`, `Semestre`, `Primera_Materia`, `Segunda_Materia`, `Tercera_Materia`, `Cuarta_Materia`, `Quinta_Materia`, `Sexta_Materia`, `Septima_Materia`) VALUES
+(10, 'Septimo', 'Inteligencia de Negocios', 'Inteligencia Artificial', 'Aplicaciones Web', 'Aplicaciones moviles', 'Proyectos Informaticos', 'Gestion Empresarial', 'Practicas de servicios'),
+(11, 'Octavo', 'Gobierno de Tenologia', 'Construccionde Documentos', 'Gestion dela configuracion', 'Aplicaciones moviles', 'Unidad de integracion curricular', 'Seguridad informatica', 'Practicas de servicios');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pais`
+--
+
+CREATE TABLE `pais` (
   `PaisId` int(11) NOT NULL,
   `Nombre` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `Provincias`
+-- Estructura de tabla para la tabla `provincias`
 --
 
-CREATE TABLE `Provincias` (
+CREATE TABLE `provincias` (
   `ProvinciasId` int(11) NOT NULL,
   `Nombre` text NOT NULL,
   `PaisesId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `provincias`
+--
+
+INSERT INTO `provincias` (`ProvinciasId`, `Nombre`, `PaisesId`) VALUES
+(0, 'ESMERALDAS', 0);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `Usuarios`
+-- Estructura de tabla para la tabla `usuarios`
 --
 
-CREATE TABLE `Usuarios` (
+CREATE TABLE `usuarios` (
   `UsuarioId` int(11) NOT NULL,
   `Cedula` varchar(17) NOT NULL,
   `Nombres` varchar(100) NOT NULL,
@@ -41,38 +92,30 @@ CREATE TABLE `Usuarios` (
   `Correo` varchar(150) NOT NULL,
   `Contrasenia` text NOT NULL,
   `Rol` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Volcado de datos para la tabla `Usuarios`
+-- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `Usuarios` (`UsuarioId`, `Cedula`, `Nombres`, `Apellidos`, `Telefono`, `Correo`, `Contrasenia`, `Rol`) VALUES
-(1, '1803971371', 'Luis Antonio', 'Llerena Ocaña', '0987654321', 'lleroc1@gmail.com', '123', 'Administrador'),
-(2, '1234567890', 'Otro Luis', 'Otro Llerena', '0987654321', 'correo@gmail.com', '123', 'Vendedor'),
-(4, '1803971330', 'Luis Antonio', 'Llerena Ocaña', '0981030167', 'lleroc@gmail.com', '123', 'Administrador');
+INSERT INTO `usuarios` (`UsuarioId`, `Cedula`, `Nombres`, `Apellidos`, `Telefono`, `Correo`, `Contrasenia`, `Rol`) VALUES
+(1, '08803564775', 'Josue Camelo', 'Rivera Caicedo', '0985246751', 'correo@gmail.com', '12345', 'Administrador'),
+(2, '020803563', 'Dc', 'Torres', '0967201167', 'vendedor@gmail.com', '123', 'Gerente');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `Pais`
+-- Indices de la tabla `malla_curricular`
 --
-ALTER TABLE `Pais`
-  ADD PRIMARY KEY (`PaisId`);
+ALTER TABLE `malla_curricular`
+  ADD PRIMARY KEY (`Id_Materias`);
 
 --
--- Indices de la tabla `Provincias`
+-- Indices de la tabla `usuarios`
 --
-ALTER TABLE `Provincias`
-  ADD PRIMARY KEY (`ProvinciasId`),
-  ADD KEY `Pais_Provincia` (`PaisesId`);
-
---
--- Indices de la tabla `Usuarios`
---
-ALTER TABLE `Usuarios`
+ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`UsuarioId`);
 
 --
@@ -80,30 +123,18 @@ ALTER TABLE `Usuarios`
 --
 
 --
--- AUTO_INCREMENT de la tabla `Pais`
+-- AUTO_INCREMENT de la tabla `malla_curricular`
 --
-ALTER TABLE `Pais`
-  MODIFY `PaisId` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `malla_curricular`
+  MODIFY `Id_Materias` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT de la tabla `Provincias`
+-- AUTO_INCREMENT de la tabla `usuarios`
 --
-ALTER TABLE `Provincias`
-  MODIFY `ProvinciasId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `Usuarios`
---
-ALTER TABLE `Usuarios`
-  MODIFY `UsuarioId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `Provincias`
---
-ALTER TABLE `Provincias`
-  ADD CONSTRAINT `Pais_Provincia` FOREIGN KEY (`PaisesId`) REFERENCES `Pais` (`PaisId`);
+ALTER TABLE `usuarios`
+  MODIFY `UsuarioId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
